6e4ecbb68c2abbbbd240398ee6128b5355b37cad
Isaac Elesia <isaacelesia@gmail.com>
2021-08-24 16:49:19 -0500

    init commit

---
+++ assets/application.js
@@ -0,0 +1,11 @@
+// Put your application javascript here
+
+// Collection filter
+if (document.getElementById('sort_by') !== null) {
+	document.querySelector('#sort_by').addEventListener('change', function (e) {
+		let url = new URL(window.location.href);
+		url.searchParams.set('sort_by', e.currentTarget.value);
+
+		window.location = url.href;
+	});
+}
